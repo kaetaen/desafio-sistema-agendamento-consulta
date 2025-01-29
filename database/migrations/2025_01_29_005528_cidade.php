62682906d9136d825575f71948f462dd658ae290
kaetaen <rubens.santos@hurb.com>
2025-01-28 22:30:24 -0300

    feat: add migration to table 'cidade'

---
+++ database/migrations/2025_01_29_005528_cidade.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cidade', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome');
+            $table->string('estado', length: 2);
+            $table->timestamps();
+            $table->softDeletes('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cidade');
+    }
+};
