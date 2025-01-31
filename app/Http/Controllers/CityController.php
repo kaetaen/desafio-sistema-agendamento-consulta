0b12d97f4a5dc5827fee31f168b95ee47147e3a9
kaetaen <rubens.santos@hurb.com>
2025-01-30 22:48:38 -0300

    feat: create CityController

---
+++ app/Http/Controllers/CityController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\City;
+use Exception;
+
+class CityController extends Controller
+{
+
+    use \App\Traits\HandleExceptions;
+
+    public function index()
+    {
+        try {
+            $cities = City::all();
+            return response()->json($cities, 200);
+
+        } catch (Exception $e) {
+            $error = "An error occurred when trying to list cities";
+            return $this->handleException($e, 500, $error);
+        }
+    }
+}
