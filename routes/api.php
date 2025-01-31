4301b2b036dd0cef98d42f3b4af8c79c33e85964
kaetaen <rubens.santos@hurb.com>
2025-01-30 22:50:01 -0300

    feat: add route for retrieving cities in API

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CityController;
 
 Route::group([
 
@@ -16,4 +17,7 @@ Route::group([
     Route::get('user', [AuthController::class, 'getUser']);
     Route::post('register', [AuthController::class, 'register']);
 });
+
+Route::get('cidades', [CityController::class, 'index']);
+
 
