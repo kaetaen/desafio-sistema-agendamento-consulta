27b63d77e6768f2c59ef42d9b98ff4c2f68536c9
kaetaen <rubens.santos@hurb.com>
2025-01-31 17:31:02 -0300

    feat: add method to retrieve doctors by city and refactoring

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Doctor;
+use Illuminate\Http\Request;
 use App\Http\Requests\DoctorRequest;
 use Illuminate\Support\Str;
 
@@ -16,17 +17,15 @@ class DoctorController extends Controller
     public function index(DoctorRequest $request)
     {
         try {
-            $mapDoctorPrefix = ['dra.', 'dr.', 'dra', 'dr'];
-            $doctorName = $request->nome;
-            $exactDoctorName = '';
-
-            foreach ($mapDoctorPrefix as $prefix) {
-                $exactDoctorName = Str::replace($prefix, '', $doctorName, caseSensitive: false);
-            }
-            
-            $doctors = Doctor::where('nome', 'like', "%{$exactDoctorName}%")
+            $doctorName = $this->removeDoctorPrefix($request->nome);
+            $doctors = Doctor::where('nome', 'like', "%{$doctorName}%")
                 ->orderBy('nome', 'ASC')
                 ->get();
+
+            if ($doctors->isEmpty()) {
+                return response()->json(['message' => 'No doctors found'], 404);
+            }
+            
             return response()->json($doctors, 200);
         }
         catch (\Exception $e) {
@@ -53,5 +52,41 @@ class DoctorController extends Controller
             $message= 'Fails when try to create a new doctor';
             return $this->handleException($e, 500, $message);
         }
+    }
+
+    public function getDoctorsByCity(DoctorRequest $request, $id_cidade)
+    {;
+        try {
+            $doctorName = $this->removeDoctorPrefix($request->nome);
+            $doctors = Doctor::where('cidade_id', $id_cidade)
+                ->where('nome', 'like', "%{$doctorName}%")
+                ->orderBy('nome', 'ASC')
+                ->get();
+
+            if ($doctors->isEmpty()) {
+                return response()->json(['message' => 'No doctors found'], 404);
+            }
+
+            return response()->json($doctors, 200);
+        }
+        catch (\Exception $e) {
+            $message = 'An error occurred when trying to list doctors by city';
+            return $this->handleException($e, 500, $message);
+        }
+    }
+
+    private function removeDoctorPrefix(string|null $doctorName): string
+    {
+        if (empty($doctorName)) {
+            return '';
+        }
+        $mapDoctorPrefix = ['dra.', 'dr.', 'dra', 'dr'];
+        $exactDoctorName = '';
+
+        foreach ($mapDoctorPrefix as $prefix) {
+            $exactDoctorName = Str::replace($prefix, '', $doctorName, caseSensitive: false);
+        }
+
+        return $exactDoctorName;
     }
 }
