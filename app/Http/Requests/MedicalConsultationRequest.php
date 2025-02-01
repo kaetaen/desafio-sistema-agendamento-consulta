946b6ac83068c4f398fb89b81031924b23261c3d
kaetaen <rubens.santos@hurb.com>
2025-02-01 13:19:13 -0300

    feat: add MedicalConsultationRequest for validation of consultation data

---
+++ app/Http/Requests/MedicalConsultationRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class MedicalConsultationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "medico_id" => "required|integer|exists:medico,id",
+            "paciente_id" =>   "required|integer|exists:paciente,id",
+            "data" => "required|date_format:Y-m-d H:i:s|after:now"
+        ];
+    }
+}
