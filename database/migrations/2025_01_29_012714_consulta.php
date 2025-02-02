e8a89b09b23db2bac09bd8ebeb71fe580949957b
kaetaen <rubens.santos@hurb.com>
2025-02-01 22:19:55 -0300

    feat: rename migration

--- database/migrations/2025_01_29_012714_consulta.php
+++ database/migrations/2025_01_29_012714_consulta.php
@@ -13,11 +13,14 @@ return new class extends Migration
     {
         Schema::create('consulta', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->bigInteger('medico_id');
-            $table->bigInteger('paciente_id');
+            $table->unsignedbigInteger('medico_id');
+            $table->unsignedbigInteger('paciente_id');
             $table->timestamp('data');
             $table->timestamps();
             $table->softDeletes('deleted_at');
+
+            $table->foreign('medico_id')->references('id')->on('medico')->onDelete('cascade');
+            $table->foreign('paciente_id')->references('id')->on('paciente')->onDelete('cascade');
         });
     }
 
